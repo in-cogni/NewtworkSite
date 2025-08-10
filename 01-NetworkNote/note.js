a834d33d13412eec1a7ee2017d3150c2af2bdd4c
LadaKashubo <lkashubo@yandex.ru>
2025-08-11 01:09:52 +0300

    maintenance

--- 01-NetworkNote/note.js
+++ 01-NetworkNote/note.js
@@ -1,4 +1,4 @@
-// JavaScript source code
+﻿// JavaScript source code
 function SwitchBackground()
 {
     let switchButton = document.getElementById("switchBackground");
@@ -6,4 +6,13 @@ function SwitchBackground()
     document.body.style.transition = `background ${delay}s ease, color ${delay}s ease`;
     switchButton.style.transition = `background-image ${delay}s ease, filter ${delay}s ease`;
     document.body.className = document.body.className === "light" ? "dark" : "light";
+}
+function btnContent()
+{
+    let caret = document.getElementById("caret");
+    let textContent = document.getElementById("textContent");
+
+    const isHidden = window.getComputedStyle(textContent).display === 'none';
+    textContent.style.display = isHidden ? 'block' : 'none';
+    caret.style.transform = isHidden ? `rotate(180deg)` : `rotate(0deg)`;
 }
